de788bcfaae03b04216eaa68da0a12f885f21166
Saml592 <mogakasamwel592@gmail.com>
2026-06-19 12:20:27 +0300

    M

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AIM: <code>Better to learn</code> and improve your skills
         </p>
         <a
           className="App-link"
